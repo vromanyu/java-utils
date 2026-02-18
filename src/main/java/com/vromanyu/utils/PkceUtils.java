bb7085ee6c31d96860a7d3a780da2ad4fb68ea9c
vromanyu <vromanyu@outlook.com>
2026-02-18 20:58:28 +0200

    add javadoc

--- src/main/java/com/vromanyu/utils/PkceUtils.java
+++ src/main/java/com/vromanyu/utils/PkceUtils.java
@@ -8,6 +8,11 @@ import java.util.Base64;
 
 public final class PkceUtils {
 
+    /**
+     * @param size random bytes size. Must be between 32 and 64.
+     * @return code verifier as Base64 URL encoded string
+     * @implNote uses SecureRandom to generate the random bytes.
+     */
  public static String generateCodeVerifier(int size) {
   if (size < 32 || size > 64) {
    throw new IllegalArgumentException("size must be between 32 and 64");
@@ -18,6 +23,12 @@ public final class PkceUtils {
   return Base64.getUrlEncoder().withoutPadding().encodeToString(verifier);
  }
 
+    /**
+     * @param codeVerifier The generated code verifier.
+     * @return code challenge as Base64 URL encoded string
+     * @throws NoSuchAlgorithmException if SHA-256 algorithm is not supported
+     * @implNote gets the ASCII encoded bytes of the code verifier and hashes them using SHA-256.
+     */
  public static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
   if (codeVerifier == null) {
    throw new IllegalArgumentException("code verifier must not be null");
