31ef3fe39f046e2010c0c744c83b5e44bda5d936
vromanyu <vromanyu@outlook.com>
2026-02-18 21:17:47 +0200

    reformat code and make constructor private

--- src/main/java/com/vromanyu/utils/PkceUtils.java
+++ src/main/java/com/vromanyu/utils/PkceUtils.java
@@ -8,20 +8,23 @@ import java.util.Base64;
 
 public final class PkceUtils {
 
+    private PkceUtils() {
+    }
+
     /**
      * @param size random bytes size. Must be between 32 and 64.
      * @return code verifier as Base64 URL encoded string
      * @implNote uses SecureRandom to generate the random bytes.
      */
- public static String generateCodeVerifier(int size) {
-  if (size < 32 || size > 64) {
-   throw new IllegalArgumentException("size must be between 32 and 64");
-  }
-  SecureRandom secureRandom = new SecureRandom();
-  byte[] verifier = new byte[size];
-  secureRandom.nextBytes(verifier);
-  return Base64.getUrlEncoder().withoutPadding().encodeToString(verifier);
- }
+    public static String generateCodeVerifier(int size) {
+        if (size < 32 || size > 64) {
+            throw new IllegalArgumentException("size must be between 32 and 64");
+        }
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] verifier = new byte[size];
+        secureRandom.nextBytes(verifier);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(verifier);
+    }
 
     /**
      * @param codeVerifier The generated code verifier.
@@ -29,14 +32,14 @@ public final class PkceUtils {
      * @throws NoSuchAlgorithmException if SHA-256 algorithm is not supported
      * @implNote gets the ASCII encoded bytes of the code verifier and hashes them using SHA-256.
      */
- public static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
-  if (codeVerifier == null) {
-   throw new IllegalArgumentException("code verifier must not be null");
-  }
-  byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
-  MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-  messageDigest.update(bytes, 0, bytes.length);
-  byte[] digest = messageDigest.digest();
-  return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
- }
+    public static String generateCodeChallenge(String codeVerifier) throws NoSuchAlgorithmException {
+        if (codeVerifier == null) {
+            throw new IllegalArgumentException("code verifier must not be null");
+        }
+        byte[] bytes = codeVerifier.getBytes(StandardCharsets.US_ASCII);
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        messageDigest.update(bytes, 0, bytes.length);
+        byte[] digest = messageDigest.digest();
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(digest);
+    }
 }
